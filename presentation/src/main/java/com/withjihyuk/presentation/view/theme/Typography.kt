1e8b9f129828e02497e2ab4e09ad25b645e998ff
Jihyuk Lee <lead@sver.dev>
2024-10-10 18:02:49 +0900

    add :: 가변 폰트로 변경

---
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
@@ -0,0 +1,11 @@
+package com.withjihyuk.presentation.view.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.withjihyuk.presentation.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard, FontWeight.Bold, FontStyle.Normal)
+)
