70902ee55c16fd4d9115f37bf98bccd39703f1bc
Jihyuk Lee <lead@sver.dev>
2024-10-14 23:37:26 +0900

    design :: theme 추가

---
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Colors.kt
@@ -0,0 +1,6 @@
+package com.withjihyuk.presentation.view.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CardBackgroundColor = Color(0xFF434343)
+val MainBackgroundColor = Color(0xFF2A2A2A)
--- presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
@@ -1,11 +1,42 @@
 package com.withjihyuk.presentation.view.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.withjihyuk.presentation.R
 
-val pretendard = FontFamily(
-    Font(R.font.pretendard, FontWeight.Bold, FontStyle.Normal)
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontSize = 24.sp,
+        lineHeight = 31.2.sp,
+        fontFamily = FontFamily(Font(R.font.pretendard)),
+        fontWeight = FontWeight(600),
+    ),
+    labelLarge = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 20.8.sp,
+        fontFamily = FontFamily(Font(R.font.pretendard)),
+        fontWeight = FontWeight(600),
+    ),
+    bodySmall = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 18.2.sp,
+        fontFamily = FontFamily(Font(R.font.pretendard)),
+        fontWeight = FontWeight(400),
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 20.8.sp,
+        fontFamily = FontFamily(Font(R.font.pretendard)),
+        fontWeight = FontWeight(400),
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontFamily = FontFamily(Font(R.font.pretendard)),
+        fontWeight = FontWeight(500),
+    )
 )
