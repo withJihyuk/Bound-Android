c54486fac077f8012b68c2ac45f17bf498e55a07
Jihyuk Lee <lead@sver.dev>
2024-10-15 21:05:08 +0900

    chore :: 가변 폰트에서 일반 폰트로 변경

--- presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
@@ -4,38 +4,40 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.withjihyuk.presentation.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_regular),
+    Font(R.font.pretendard_semibold),
+    Font(R.font.pretendard_extrabold),
+    Font(R.font.pretendard_medium),
+)
 
 val Typography = Typography(
     titleLarge = TextStyle(
         fontSize = 32.sp,
         lineHeight = 41.6.sp,
-        fontFamily = FontFamily(Font(R.font.pretendard, weight = FontWeight.W900)),
+        fontFamily = pretendard
     ),
     labelLarge = TextStyle(
         fontSize = 16.sp,
         lineHeight = 20.8.sp,
-        fontFamily = FontFamily(Font(R.font.pretendard)),
-        fontWeight = FontWeight(600),
+        fontFamily = pretendard
     ),
     bodySmall = TextStyle(
         fontSize = 14.sp,
         lineHeight = 18.2.sp,
-        fontFamily = FontFamily(Font(R.font.pretendard)),
-        fontWeight = FontWeight(400),
+        fontFamily = pretendard
     ),
     bodyMedium = TextStyle(
         fontSize = 16.sp,
         lineHeight = 20.8.sp,
-        fontFamily = FontFamily(Font(R.font.pretendard)),
-        fontWeight = FontWeight(400),
+        fontFamily = pretendard
     ),
     bodyLarge = TextStyle(
         fontSize = 20.sp,
         lineHeight = 26.sp,
-        fontFamily = FontFamily(Font(R.font.pretendard)),
-        fontWeight = FontWeight(500),
+        fontFamily = pretendard
     )
 )
