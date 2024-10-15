2fe36b03873ce95453ff006505c97f757d93d019
Jihyuk Lee <lead@sver.dev>
2024-10-15 14:09:22 +0900

    design :: 디자인상의 회색 디자인 시스템 추가

--- presentation/src/main/java/com/withjihyuk/presentation/view/theme/Colors.kt
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Colors.kt
@@ -4,3 +4,15 @@ import androidx.compose.ui.graphics.Color
 
 val CardBackgroundColor = Color(0xFF434343)
 val MainBackgroundColor = Color(0xFF2A2A2A)
+
+object GrayStyles {
+    val gray01: Color = Color(0xFFF9F9F9)
+    val gray02: Color = Color(0xFFECECEC)
+    val gray03: Color = Color(0xFFD5D5D5)
+    val gray04: Color = Color(0xFFBDBDBD)
+    val gray05: Color = Color(0xFFABABAB)
+    val gray06: Color = Color(0xFFA7A7A7)
+    val gray07: Color = Color(0xFF7B7B7B)
+    val gray08: Color = Color(0xFF6C6C6C)
+    val gray09: Color = Color(0xFF434343)
+}
