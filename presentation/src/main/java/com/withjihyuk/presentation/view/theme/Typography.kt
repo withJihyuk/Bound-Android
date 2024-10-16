66ef074db99a0a037617c5b257f05efa93544325
Jihyuk Lee <lead@sver.dev>
2024-10-16 16:15:00 +0900

    design :: 새로운 폰트 Style 추가

--- presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
+++ presentation/src/main/java/com/withjihyuk/presentation/view/theme/Typography.kt
@@ -4,21 +4,35 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.withjihyuk.presentation.R
 
 val pretendard = FontFamily(
-    Font(R.font.pretendard_regular),
-    Font(R.font.pretendard_semibold),
-    Font(R.font.pretendard_extrabold),
-    Font(R.font.pretendard_medium),
+    Font(R.font.pretendard_regular, weight = FontWeight.Normal),
+    Font(R.font.pretendard_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.pretendard_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.pretendard_medium, weight = FontWeight.Medium),
 )
 
-val Typography = Typography(
+val changone = FontFamily(
+    Font(R.font.changone, weight = FontWeight.Bold)
+)
+
+val boundTypography = Typography(
     titleLarge = TextStyle(
         fontSize = 32.sp,
         lineHeight = 41.6.sp,
-        fontFamily = pretendard
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = 0.5.sp,
+    ),
+    titleMedium = TextStyle(
+        fontSize = 32.sp,
+        lineHeight = 41.6.sp,
+        fontFamily = changone,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.5.sp,
     ),
     labelLarge = TextStyle(
         fontSize = 16.sp,
